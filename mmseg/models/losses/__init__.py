74ba21d4231fcc9ad3371464345ffd391f39ce23
csj <954323022@qq.com>
2023-03-30 23:25:19 +0800

    fix dice loss

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
-                                 cross_entropy, mask_cross_entropy)
+                                   cross_entropy, mask_cross_entropy)
 from .dice_loss import DiceLoss
 from .focal_loss import FocalLoss
 from .lovasz_loss import LovaszLoss
